3fde721a1f5c448793e913237090eefefcfe7c67
Clay Battle <cbattle1@binghamton.edu>
2024-09-28 18:56:40 -0400

    Finish example port

--- python_port/Pendulum_energy_example.py
+++ python_port/Pendulum_energy_example.py
@@ -14,34 +14,21 @@ n = 100
 θ_range = range(-3*math.pi, stop=3*math.pi, step=n)
 ω_range = range(-10, stop=10, step=n)
 
-# Creating a grid of theta and omega values and calculating energy
-V_values = pendulum_energy(θ, ω, m, L, g) for θ in θ_range ω in ω_range
+# Creating a grid of theta and omega values and calculating energy. 
+#TODO optimize with numpy
+V_values = []
+for θ in θ_range:
+    for ω in ω_range:
+        V_values.append(pendulum_energy(θ, ω, m, L, g))
 
 # 3D Plotting
 # adjusted_angle = sol[1, :] .- θ₁
-surface(ω_range, θ_range, V_values, title="Pendulum Energy", xlabel="ω (rad/s)", ylabel="θ (rad)", zlabel="Energy (Joules)")
 
-
-
-
-'''
-# Number of points and ranges
-n = 100
-theta_range = np.linspace(-3 * np.pi, 3 * np.pi, n)
-omega_range = np.linspace(-10, 10, n)
-
-# Creating a grid of theta and omega values
-theta_grid, omega_grid = np.meshgrid(theta_range, omega_range)
-
-# Calculating energy values
-V_values = pendulum_energy(theta_grid, omega_grid, m, L, g)
-
-# 3D Plotting
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Plot surface
-surf = ax.plot_surface(omega_grid, theta_grid, V_values, cmap='viridis')
+surf = ax.plot_surface(ω_range, θ_range, V_values, cmap='viridis')
 
 # Adding labels and title
 ax.set_title("Pendulum Energy")
@@ -54,4 +41,3 @@ fig.colorbar(surf)
 
 # Display plot
 plt.show()
-'''
