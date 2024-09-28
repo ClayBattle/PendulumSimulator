d8cc179b4c16546ab0200bc6df986c1410ef8887
Clay Battle <cbattle1@binghamton.edu>
2024-09-28 19:23:13 -0400

    Pendulum can generate plot (?)

--- python_port/Pendulum_energy_example.py
+++ python_port/Pendulum_energy_example.py
@@ -3,23 +3,37 @@ import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pendulum_energy
+import numpy as np
 
 # Parameters
 m = 1
 L = 9.8
 g = 9.8
 
-# Number of points and ranges
-n = 100
-θ_range = range(-3*math.pi, stop=3*math.pi, step=n)
-ω_range = range(-10, stop=10, step=n)
 
-# Creating a grid of theta and omega values and calculating energy. 
-#TODO optimize with numpy
-V_values = []
-for θ in θ_range:
-    for ω in ω_range:
-        V_values.append(pendulum_energy(θ, ω, m, L, g))
+n = 100
+θ_range = np.linspace(-3 * math.pi, 3 * math.pi, n)  # Using linspace for floating-point values
+ω_range = np.linspace(-10, 10, n)
+
+# Creating a grid of theta and omega values
+θ_grid, ω_grid = np.meshgrid(θ_range, ω_range)
+
+# Vectorized calculation of pendulum energy over the entire grid
+pendulumCalc = np.vectorize(pendulum_energy.pendulum_energy)
+V_values = pendulumCalc(θ_grid, ω_grid, m, L, g)
+
+
+# # Number of points and ranges
+# n = 100
+# θ_range = range(int(-3*math.pi), int(3*math.pi), n)
+# ω_range = range(-10, 10, n)
+
+# # Creating a grid of theta and omega values and calculating energy. 
+# #TODO optimize with numpy
+# V_values = []
+# for θ in θ_range:
+#     for ω in ω_range:
+#         V_values.append(pendulum_energy.pendulum_energy(θ, ω, m, L, g))
 
 # 3D Plotting
 # adjusted_angle = sol[1, :] .- θ₁
--- python_port/pendulum_energy.py
+++ python_port/pendulum_energy.py
@@ -2,5 +2,5 @@ import math
 
 # Function to calculate the total energy
 def pendulum_energy(θ, ω, m, L, g):
-    V = 0.5 * m * (L * ω)^2 + m * g * L * (1 - math.cos(θ))
+    V = 0.5 * m * (L * ω)**2 + m * g * L * (1 - math.cos(θ))
     return V
